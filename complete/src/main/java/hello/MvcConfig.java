89e152f5b3ef3f4ca9864f447c7ede3e8a7c6854
James Murphy <murphyjiv@gmail.com>
2018-10-30 10:34:27 -0400

    First commit

--- complete/src/main/java/hello/MvcConfig.java
+++ complete/src/main/java/hello/MvcConfig.java
@@ -8,10 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/home").setViewName("home");
-        registry.addViewController("/").setViewName("home");
-        registry.addViewController("/hello").setViewName("hello");
+        // registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("login");
+        // registry.addViewController("/hello").setViewName("hello");
         registry.addViewController("/login").setViewName("login");
     }
-
+@Configuration
+@EnableWebMvc
+@ComponentScan
+public class ServerConfiguration extends WebMvcAutoConfiguration{
+}
 }
--- complete/src/main/java/hello/WebSecurityConfig.java
+++ complete/src/main/java/hello/WebSecurityConfig.java
@@ -17,7 +17,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
+		  	.antMatchers("/login").permitAll()
+			// .antMatchers("/resources/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
